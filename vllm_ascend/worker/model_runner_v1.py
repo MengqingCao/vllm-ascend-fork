3bf09d6ed5a1d5dc8d93053f525398c8a1312b51
MengqingCao <cmq0113@163.com>
2025-12-02 13:23:47 +0000

    fix pooling mask
    Signed-off-by: MengqingCao <cmq0113@163.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -981,7 +981,7 @@ class NPUModelRunner(LoRAModelRunnerMixin):
         # Pooling situation.
         if self.model_config.runner_type == "pooling":
             return self.attn_mask_builder.get_attn_mask(
-                2048, self.dtype, self.device)
+                2048, torch.bool, self.device)
         return self.attn_mask_builder.get_splitfuse_attn_mask()
 
     def _calc_mrope_positions(self, scheduler_output: "SchedulerOutput"):
